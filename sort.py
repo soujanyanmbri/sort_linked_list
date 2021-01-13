c2f0c0644d4739734c8c46854b124f207628de3b
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2021-01-13 20:48:36 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,15 @@
+NumList = []
+
+Number = int(input("Enter the number of elements in the list: "))
+for i in range(1, Number + 1):
+    value = int(input("Please enter the Value of %d Element : " %i))
+    NumList.append(value)
+
+for i in range (Number):
+    for j in range(i + 1, Number):
+        if(NumList[i] > NumList[j]):
+            temp = NumList[i]
+            NumList[i] = NumList[j]
+            NumList[j] = temp
+
+print("Element After Sorting List in Ascending Order is : ", NumList)
